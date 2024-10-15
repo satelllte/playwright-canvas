22697cad177bf918f01af9d7ca2cef267bfeb80f
Yehor <20585619+satelllte@users.noreply.github.com>
2024-10-15 23:15:50 +0300

    stabilize test by consuming "fastForward"

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -10,17 +10,17 @@ test('moves', async ({page}) => {
   await page.goto('/');
 
   await page.clock.pauseAt(0);
-  await page.clock.runFor(1000);
+  await page.clock.fastForward(1000);
 
   await expect(page).toHaveScreenshot();
 
   await page.keyboard.down('ArrowRight');
-  await page.clock.runFor(500);
+  await page.clock.fastForward(500);
 
   await expect(page).toHaveScreenshot();
 
   await page.keyboard.down('ArrowDown');
-  await page.clock.runFor(500);
+  await page.clock.fastForward(500);
 
   await page.keyboard.up('ArrowRight');
   await page.keyboard.up('ArrowDown');
